004f65c8121698ccfc9b9c19379af654f192e506
heongle <39309769+heongle@users.noreply.github.com>
2021-12-31 18:48:05 +0800

    Add dynamic top bar title

--- app/src/main/java/com/yumenonaka/ymnkapp/MainActivity.kt
+++ app/src/main/java/com/yumenonaka/ymnkapp/MainActivity.kt
@@ -7,11 +7,14 @@ import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.stringResource
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import com.yumenonaka.ymnkapp.components.Drawer
+import com.yumenonaka.ymnkapp.models.app.RouteData
 import com.yumenonaka.ymnkapp.ui.theme.YumenoNakaAppTheme
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
@@ -30,7 +33,7 @@ class MainActivity : ComponentActivity() {
                     drawerContent = { Drawer(routes, navController, scaffoldState, coroutineScope) },
                     drawerShape = MaterialTheme.shapes.large,
                     drawerGesturesEnabled = true,
-                    topBar = { YumenoTopBar(scaffoldState, coroutineScope) },
+                    topBar = { YumenoTopBar(scaffoldState, coroutineScope, navController, routes) },
                     content = { Router(routes, navController) }
                 )
             }
@@ -39,11 +42,18 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun YumenoTopBar(scaffoldState: ScaffoldState, coroutineScope: CoroutineScope) {
+fun YumenoTopBar(
+    scaffoldState: ScaffoldState,
+    coroutineScope: CoroutineScope,
+    navController: NavHostController,
+    routes: List<RouteData>
+) {
+    val navBackStackEntry by navController.currentBackStackEntryAsState()
+    val currentDestination = navBackStackEntry?.destination
     TopAppBar(
         backgroundColor = Color(0xff8445de),
         contentColor = Color.White,
-        title = { Text(text = stringResource(R.string.app_name)) },
+        title = { Text(text = routes.find { it.route == currentDestination?.route }?.name ?: "") },
         navigationIcon = {
             IconButton(
                 onClick = {
